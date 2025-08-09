dfdb17aacf62f393222e33263f28c2c86126400d
Doeun <68941249+RKDLDE@users.noreply.github.com>
2025-08-10 00:04:36 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

---
+++ Python/백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+stack = []
+
+for _ in range(n):
+    command = input().strip()
+
+    if command.startswith("push"):
+        _, value = command.split()
+        stack.append(int(value))
+        
+    elif command == "pop":
+        print(stack.pop(-1) if stack else -1)
+        
+    elif command == "size":
+        print(len(stack))
+        
+    elif command == "empty":
+        print(0 if stack else 1)
+        
+    elif command == "top":
+        print(stack[-1] if stack else -1)
