462f1d60d8129557f7622e84d190206fc50a28bf
Doeun <68941249+RKDLDE@users.noreply.github.com>
2025-09-08 15:10:41 +0900

    [level 1] Title: 행렬의 덧셈, Time: 40.56 ms, Memory: 21.9 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -0,0 +1,9 @@
+def solution(arr1, arr2):
+    answer = []
+    for i in range(len(arr1)):
+        add = []
+        for j in range(len(arr1[i])):
+            add.append(arr1[i][j] + arr2[i][j])
+        answer.append(add)
+
+    return answer
