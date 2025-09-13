daf0978849e302deb89bd3dac665aa977f5efea9
Doeun <68941249+RKDLDE@users.noreply.github.com>
2025-09-13 15:18:36 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 2628 ms, Memory: 42656 KB -BaekjoonHub

---
+++ Python/백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,14 @@
+n = int(input())
+log = set()
+
+for _ in range(n):
+    name, eorl = input().split()
+
+    if eorl == 'enter':
+        log.add(name)
+
+    else:
+        log.remove(name)
+
+for name in sorted(log, reverse=True):
+    print(name)
